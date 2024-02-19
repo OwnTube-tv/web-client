4c8256a7db12263e1511ba8c2c8675184e75ca1d
Mats Blomdahl's Bot Account <118663939+mblomdahl-bot@users.noreply.github.com>
2024-02-19 13:22:52 +0000

    Add a commit link to see what build is deployed #14
    Local runtime:
    1. An extra `build-info.json` is added in the source root with mock info
    2. Development builds compile without further workarounds
    
    Deployed runtime:
    1. When triggering a build, GitHub Actions extracts the info of interest
    2. Build information is injected in the React project, overwriting mock data
    3. The commit and build timestamp is stamped into the compiled build
    4. Build output is uploaded and published to GitHub Pages
    
    (@tryklick was here.)

--- OwnTube.tv/App.tsx
+++ OwnTube.tv/App.tsx
@@ -1,10 +1,13 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
 
+import build_info from './build-info.json';
+
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
+      <Text>Open up App.tsx to start working on your app, current deployed revision is <a href={build_info.COMMIT_URL} target="_blank">{build_info.GITHUB_SHA_SHORT}</a> built at {build_info.BUILD_TIMESTAMP}.</Text>
+      <hr></hr>
       <Text>(Your friendly <a href="https://github.com/ar9708" target="_blank"><code>ar9708</code></a> 🙋‍♀️ was here!)</Text>
       <StatusBar style="auto" />
     </View>

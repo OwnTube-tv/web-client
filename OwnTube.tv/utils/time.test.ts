df0dec0fdb071cef9586301f13c6ca8c7b3145d7
Mykhailo Danilenko <mykhailo.danilenko@gmail.com>
2024-05-28 10:45:02 +0300

    add scrubbar jump to point in video

---
+++ OwnTube.tv/utils/time.test.ts
@@ -0,0 +1,11 @@
+import { getHumanReadableDuration } from "./time";
+
+describe("getHumanReadableDuration", () => {
+  it("should output 40m30s for 2_430_000 ms input", () => {
+    expect(getHumanReadableDuration(2_430_000)).toBe("40:30");
+  });
+
+  it("should output 1h34m25s for 5_665_000 ms input", () => {
+    expect(getHumanReadableDuration(5_665_000)).toBe("01:34:25");
+  });
+});
---
+++ OwnTube.tv/utils/time.ts
@@ -0,0 +1,9 @@
+export const getHumanReadableDuration = (ms: number) => {
+  const seconds = Math.floor((ms / 1000) % 60);
+  const minutes = Math.floor((ms / 1000 / 60) % 60);
+  const hours = Math.floor((ms / 1000 / 3600) % 24);
+
+  const mmss = `${String(minutes).padStart(2, "0")}:${String(seconds).padStart(2, "0")}`;
+
+  return hours > 0 ? `${String(hours).padStart(2, "0")}:${mmss}` : mmss;
+};

44155b40a7415e14a4ee1d59c12e2d852232ad5c
Mats Blomdahl <mats.blomdahl@gmail.com>
2024-09-16 11:41:10 +0200

    Send User-Agent as a custom header (#157) (#158)

--- OwnTube.tv/api/axiosInstance.ts
+++ OwnTube.tv/api/axiosInstance.ts
@@ -1,4 +1,5 @@
 import axios, { AxiosInstance } from "axios";
+import build_info from "../build-info.json";
 
 export abstract class AxiosInstanceBasedApi {
   protected constructor(debugLogging: boolean = false) {
@@ -10,6 +11,10 @@ export abstract class AxiosInstanceBasedApi {
   // Our Axios instance, https://axios-http.com/docs/instance
   instance!: AxiosInstance;
 
+  private getAppIdentifier(): string {
+    return `OwnTube-tv/web-client@${build_info.GITHUB_SHA_SHORT} (https://github.com/${build_info.GITHUB_ACTOR})`;
+  }
+
   /**
    * Create the Axios instance with app identifier and request/response interceptors
    */
@@ -18,8 +23,7 @@ export abstract class AxiosInstanceBasedApi {
       withCredentials: false,
       headers: {
         "Access-Control-Allow-Origin": "*",
-        "Content-Type": "application/json",
-        "User-Agent": "OwnTube.tv/1.0.0 (https://app.owntube.tv)",
+        "X-App-Identifier": this.getAppIdentifier(),
       },
     });
   }

edfe3a7f0cff26ee59e8013647c7248ddf89ddd6
Mykhailo Danilenko <mykhailo.danilenko@gmail.com>
2025-01-21 20:02:52 +0200

    fix build number last digit, add base64 encoding for the jks keystore

--- OwnTube.tv/app.config.ts
+++ OwnTube.tv/app.config.ts
@@ -1,6 +1,9 @@
 const getBuildNumber = () => {
   const now = new Date();
-  return `${now.getUTCFullYear() % 100}${String(now.getUTCMonth() + 1).padStart(2, "0")}${String(now.getUTCDate()).padStart(2, "0")}${String(now.getUTCHours()).padStart(2, "0")}${String(now.getUTCMinutes()).padStart(2, "0")}`;
+  return `${now.getUTCFullYear() % 100}${String(now.getUTCMonth() + 1).padStart(2, "0")}${String(now.getUTCDate()).padStart(2, "0")}${String(now.getUTCHours()).padStart(2, "0")}${String(now.getUTCMinutes()).padStart(2, "0")}`.slice(
+    0,
+    -1,
+  );
 };
 
 export default {
@@ -29,7 +32,7 @@ export default {
     baseUrl: process.env.EXPO_PUBLIC_BASE_URL || "/web-client",
   },
   android: {
-    versionCode: Math.floor(new Date().getTime() / 1000),
+    versionCode: getBuildNumber(),
     adaptiveIcon: {
       foregroundImage:
         process.env.EXPO_PUBLIC_ANDROID_ADAPTIVE_ICON_FOREGROUND || "./assets/adaptive-icon-foreground.png",

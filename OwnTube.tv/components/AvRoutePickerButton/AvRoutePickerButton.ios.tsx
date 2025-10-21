0b096efeeff1421d122424f8ba500aee0a8c9975
Mykhailo Danilenko <mykhailo.danilenko@gmail.com>
2025-10-21 19:42:44 +0300

    fix package lock, reduce button width on mobile

--- OwnTube.tv/components/AvRoutePickerButton/AvRoutePickerButton.ios.tsx
+++ OwnTube.tv/components/AvRoutePickerButton/AvRoutePickerButton.ios.tsx
@@ -2,12 +2,19 @@ import AirPlayButton from "react-native-airplay-button";
 import PlayerButton from "../VideoControlsOverlay/components/PlayerButton";
 import { StyleSheet, View } from "react-native";
 import { AvRoutePickerButtonProps } from "./AvRoutePickerButton.web";
+import { useBreakpoints } from "../../hooks";
 
 const AvRoutePickerButton = (_props: AvRoutePickerButtonProps) => {
+  const { isMobile } = useBreakpoints();
+
   return (
-    <View style={styles.container} pointerEvents="box-none">
-      <AirPlayButton style={styles.airPlayTransparentButton} tintColor="transparent" activeTintColor="transparent" />
-      <PlayerButton icon={"AirPlay"} onPress={null} />
+    <View style={[styles.container, { width: isMobile ? 42 : 48 }]} pointerEvents="box-none">
+      <AirPlayButton
+        style={{ ...styles.airPlayTransparentButton, width: isMobile ? 42 : 48 }}
+        tintColor="transparent"
+        activeTintColor="transparent"
+      />
+      <PlayerButton reducedWidth={isMobile} icon={"AirPlay"} onPress={null} />
     </View>
   );
 };
